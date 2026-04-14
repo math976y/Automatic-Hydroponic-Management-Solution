c69865f452573217fd6de68b6c62a6056e913eba
oleboleskole3 <oleboleskole3@kvikvet.dk>
2026-04-14 11:11:51 +0200

    Start work on webinterface

---
+++ Webinterface/src/lib/slider.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let { min = 0, max = 10, value = $bindable(5), step = 0, title = $bindable("") } = $props();
+
+    let id = "slider" + Math.random();
+</script>
+
+<div class="container">
+    {#if title}
+        <label class="title" for={id}>{title}:</label>
+    {/if}
+    <input class="slider" {id} type="range" {min} {max} bind:value step={step === 0 ? 'any' : step} />
+    <input type="number" class="value" bind:value={value} />
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .slider {
+        margin: 0px 5px 0px 10px;
+    }
+    .value {
+        margin: 0px;
+        width: 8ch;
+    }
+</style>
---
+++ Webinterface/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Slider from "$lib/slider.svelte";
+
+    let val = $state(3);
+    let val2 = $state(3);
+    let val3 = $state(3);
+    let val4 = $state(3);
+</script>
+
+<h1>Drivhus kontrolpanel</h1>
+<Slider bind:value={val} step={.1} title="Værdi"/>
+<Slider bind:value={val2} step={.1} title="Værdi 2"/>
+<Slider bind:value={val3} step={.1} title="Værdi 3"/>
+<Slider bind:value={val4} step={.1} title="Værdi 4"/>
+
+<p>Slider val is {val}</p>
